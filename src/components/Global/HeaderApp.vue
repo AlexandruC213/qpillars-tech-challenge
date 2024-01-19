44baebcd19fb496f858210f4c1d82c44114c1c48
Chis Alexandru <alexandru_chis213@yahoo.com>
2024-01-19 17:46:29 +0200

    header

---
+++ src/components/Global/HeaderApp.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    <div>
+      <q-icon name="storefront" size="2rem" color="secondary" />
+      <q-img :src="logo" width="200px" class="q-ml-md" />
+    </div>
+    <div v-if="!isSmallerHeader" class="header-actions">
+      <HeaderLinks />
+    </div>
+    <q-icon
+      v-else
+      @click="showMenu = !showMenu"
+      size="2rem"
+      color="white"
+      name="img:public/icons/burger-menu.svg"
+      class="header-burger-icon"
+    />
+  </div>
+
+  <q-drawer
+    side="right"
+    v-model="showMenu"
+    :width="$q.screen.width"
+    class="drawer"
+  >
+    <q-icon
+      @click="showMenu = false"
+      class="drawer-close-icon"
+      name="close"
+      size="3rem"
+    />
+    <div class="drawer-links">
+      <HeaderLinks linkClass="q-my-lg" />
+    </div>
+  </q-drawer>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
+
+import logo from "/public/images/logo.svg";
+import HeaderLinks from "./HeaderLinks.vue";
+
+const $q = useQuasar();
+const router = useRouter();
+const showMenu = ref(false);
+
+const isSmallerHeader = computed(() => {
+  return $q.screen.width < $q.screen.sizes.md;
+});
+
+const goToLink = (link) => {
+  router.push(link);
+};
+</script>
---
+++ src/components/Global/HeaderLinks.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link
+    v-for="route in headerLinks"
+    :key="route.link"
+    :to="route.link"
+    class="header-actions-link text-body-16"
+    :class="linkClass"
+  >
+    {{ route.label }}</router-link
+  >
+  <router-link to="/register" class="header-actions-register text-body-16">
+    <q-icon
+      name="img:public/icons/user.svg"
+      color="white"
+      size="1.3rem"
+      class="q-pr-sm"
+    />
+    Sign Up
+  </router-link>
+</template>
+
+<script setup>
+const props = defineProps({
+  linkClass: {
+    type: String,
+    required: false,
+  },
+});
+
+const headerLinks = [
+  {
+    label: "Marketplace",
+    link: "/",
+  },
+  {
+    label: "Rankings",
+    link: "/rankings",
+  },
+  {
+    label: "Connect a wallet",
+    link: "/connect-wallet",
+  },
+];
+</script>
